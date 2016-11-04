65fdd1d4c08654341ff9c39f8db37be65110af0e
Azer0s <sim17057@spengergasse.at>
2016-11-04 23:13:38 +0100

    Added some features

--- StockWatch/Stock.xaml.cs
+++ StockWatch/Stock.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Windows;
 using LiveCharts;
 using LiveCharts.Wpf;
@@ -12,29 +13,42 @@ namespace StockWatch
     public partial class Stock : Window
     {
         public ChartValues<double> ChartValues;
+        public string StockTitle;
+        public LineSeries CControl;
+        public bool ShouldStop;
         public Stock(string stock)
         {
             InitializeComponent();
 
+            StockTitle = stock;
             Title = stock.Split(':')[1] + "@" + stock.Split(':')[0];
 
-            ChartValues = new ChartValues<double> {4, 6, 5, 2, 4};
+            ChartValues = new ChartValues<double> {0,0,0,0,0,0,0,0,0};
+            YAxis.Labels = new [] {""};
+
+            CControl = new LineSeries
+            {
+                Title = "",
+                Values = ChartValues
+            };
 
             Chart.Series = new SeriesCollection
             {
-                new LineSeries
-                {
-                    Title = "Price",
-                    Values = ChartValues
-                }
+                CControl
             };
 
+            ShouldStop = false;
             var stockUpdater = new Thread(() =>
             {
-                var s = new StockData(stock,this);
+                var s = new StockData(stock,this, SynchronizationContext.Current);
                 s.Run();
             });
             stockUpdater.Start();
+
+            this.Closing += delegate
+            {
+                ShouldStop = true;
+            };
         }
     }
 }
--- StockWatch/StockData.cs
+++ StockWatch/StockData.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 using LiveCharts;
+using Newtonsoft.Json;
 
 namespace StockWatch
 {
@@ -13,19 +19,51 @@ namespace StockWatch
     {
         private readonly string _stockName;
         private readonly Stock _controlller;
-        public StockData(string stock, Stock controller)
+        private readonly string _url;
+        private readonly SynchronizationContext _context;
+
+        public StockData(string stock, Stock controller, SynchronizationContext context)
         {
             _stockName = stock;
             _controlller = controller;
-
-            for (int i = 0; i < 10; i++)
-            {
-                Thread.Sleep(500);
-                _controlller.ChartValues.Add(i);
-            }
+            _url = "https://www.google.com/finance/info?q=" + _controlller.StockTitle;
+            _context = context;
         }
+
         public void Run()
         {
+            while (!_controlller.ShouldStop)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+
+                    using (var client = new HttpClient())
+                    {
+                        var result = client.GetAsync(_url);
+                        var resultAsString = result.Result.Content.ReadAsStringAsync().Result.TrimStart('/').Replace("[", "").Replace("]", "");
+                        var resAsDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(resultAsString);
+                        var value = double.Parse(resAsDict["l"].Replace(".", ","));
+
+                        var time = resAsDict["lt_dts"];
+                        Application.Current.Dispatcher.BeginInvoke(new Action(() => _controlller.CControl.Title = resAsDict["c"] + "%"));
+
+                        //TODO Implement time
+
+                        _controlller.ChartValues.Add(value);
+
+                        if (_controlller.ChartValues.Count > 10)
+                        {
+                            _controlller.ChartValues.RemoveAt(0);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    _controlller.ChartValues.Clear();
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() => _controlller.YAxis.Title = "Something went wrong!"));
+                }
+            }
         }
     }
 }
