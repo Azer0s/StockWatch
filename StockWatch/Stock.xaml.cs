b74c8d7848222432df7107a57d22d48c11e241bb
Azer0s <sim17057@spengergasse.at>
2016-11-04 21:25:09 +0100

    Added livecharts, data thread and new XAML

--- StockWatch/Stock.xaml.cs
+++ StockWatch/Stock.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using OxyPlot;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Wpf.Charts.Base;
 
 namespace StockWatch
 {
@@ -21,12 +11,30 @@ namespace StockWatch
     /// </summary>
     public partial class Stock : Window
     {
-        public IList<DataPoint> Points { get; private set; }
-
+        public ChartValues<double> ChartValues;
         public Stock(string stock)
         {
             InitializeComponent();
-            Title = "Stockwatch: " + stock;
+
+            Title = stock.Split(':')[1] + "@" + stock.Split(':')[0];
+
+            ChartValues = new ChartValues<double> {4, 6, 5, 2, 4};
+
+            Chart.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Price",
+                    Values = ChartValues
+                }
+            };
+
+            var stockUpdater = new Thread(() =>
+            {
+                var s = new StockData(stock,this);
+                s.Run();
+            });
+            stockUpdater.Start();
         }
     }
 }
--- StockWatch/StockData.cs
+++ StockWatch/StockData.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using LiveCharts;
 
 namespace StockWatch
 {
     class StockData
     {
         private readonly string _stockName;
-        public StockData(string stock)
+        private readonly Stock _controlller;
+        public StockData(string stock, Stock controller)
         {
             _stockName = stock;
+            _controlller = controller;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                _controlller.ChartValues.Add(i);
+            }
         }
         public void Run()
         {
